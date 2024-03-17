c999f0de7092af5c7aa956e7d2b3116814b2e15d
xiracuko <matvey.a.a.7@gmail.com>
2024-03-17 11:34:33 +0300

    chore: adding markup to TodoItem.tsx

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,6 +1,14 @@
 function TodoItem() {
+
   return (
-    <div>TodoItem</div>
+    <div className="todoBlock-item">
+      TodoItem
+      <div className="todoBlock-itemBtns">
+        <input type="checkbox" className="itemBtns-checkbox" id="toggle"/>
+        <label htmlFor="toggle"/>
+        <button>x</button>
+      </div>
+    </div>
   )
 }
 
