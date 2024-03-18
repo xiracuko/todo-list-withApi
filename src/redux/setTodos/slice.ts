dba197f3ef552a0c39bd69ecf31dfac734f88876
xiracuko <matvey.a.a.7@gmail.com>
2024-03-18 09:10:41 +0300

    feature: creating redux logic for todos (add/toggle/remove)

---
+++ src/redux/setTodos/slice.ts
@@ -0,0 +1,38 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+type newTodoType = {
+  id: number;
+  title: string;
+  toggle: boolean;
+};
+
+type initialStateType = {
+  todos: newTodoType[];
+};
+
+export const initialState: initialStateType = {
+  todos: [],
+};
+
+export const todoSlice = createSlice({
+  name: "todo",
+  initialState,
+  reducers: {
+    addTodo(state, action) {
+      const newTodo = {
+        id: Date.now(),
+        title: action.payload.title,
+        toggle: false,
+      };
+
+      state.todos.push(newTodo);
+    },
+    toggleTodo(state, action) {
+        const toggledTodo = state.todos.find((todo) => todo.id === action.payload.id);
+        toggledTodo ? toggledTodo.toggle = !toggledTodo.toggle : "";
+    },
+    removeTodo(state, action) {
+        state.todos = state.todos.filter((todo) => todo.id !== action.payload.id);
+    },
+  },
+});
