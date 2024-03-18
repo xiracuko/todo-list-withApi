d05522197c439492fa7938c82192752c2ff4991d
xiracuko <matvey.a.a.7@gmail.com>
2024-03-18 09:57:14 +0300

    feature: creating logic for adding todos

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,10 +1,19 @@
+import React from "react";
 import { useAppDispatch, useAppSelector } from "../hooks"
 import TodoEmpty from "./TodoEmpty"
 import TodoItem from "./TodoItem"
+import { addTodo } from "../redux/setTodos/slice";
 
 function Todo() {
+
   const dispatch = useAppDispatch();
-  const {todos} = useAppSelector((state) => state.setTodo);
+  const { todos } = useAppSelector((state) => state.setTodo);
+  const [value, setValue] = React.useState("");
+
+  const onClickAdd = (e: any) => {
+    e.preventDefault();
+    value && dispatch(addTodo({ title: value }))
+  }
 
   return (
     <div className="container">
@@ -12,8 +21,8 @@ function Todo() {
         <h2>Todo list<p>.</p></h2>
         <div className="todoBlock-info">
           <div className="formBlock">
-            <form>
-              <input type="text" />
+            <form onSubmit={onClickAdd}>
+              <input type="text" value={value} onChange={(event) => setValue(event.target.value)} />
               <button className="todoBlock-addBtn">+ add</button>
             </form>
             <button className="todoBlock-addBtnData">getData</button>
@@ -21,11 +30,6 @@ function Todo() {
           <div className="todoBlock-forAllItems">
             <h4>Your todos:</h4>
             <div className="todoBlock-forItems">
-              <TodoItem />
-              <TodoItem />
-              <TodoItem />
-              <TodoItem />
-              <TodoItem />
             </div>
             <h4 className="another">Another user's todos:</h4>
             <div className="todoBlock-forOthersItems">
